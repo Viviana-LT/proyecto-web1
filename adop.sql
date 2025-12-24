4df55431c7e8a9a0d2836c394becdeca149c594d
Haku021 <jgonzalesra@unsa.edu.pe>
2025-12-23 23:08:01 -0500

    sql

--- adop.sql
+++ adop.sql
@@ -1,7 +1,10 @@
-CREATE TABLE adopcion (
+CREATE DATABASE neko_mori;
+USE neko_mori;
+
+CREATE TABLE adopciones (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre_gato VARCHAR(50),
-    nombre_persona VARCHAR(100),
+    nombre_adoptante VARCHAR(100),
     email VARCHAR(100),
     telefono VARCHAR(20),
     direccion VARCHAR(150),
